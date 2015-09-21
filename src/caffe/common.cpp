dbdafa8bb891e9082f0f25515c082806a25beefa
yangzq08 <yzqzoom@163.com>
2015-09-21 13:49:28 +0800

    modify common.hpp & common.cpp for net bug fix

--- src/caffe/common.cpp
+++ src/caffe/common.cpp
@@ -11,7 +11,9 @@ namespace caffe {
 
 // Make sure each thread can have different values.
 static boost::thread_specific_ptr<Caffe> thread_instance_;
-
+// -----------------------------NOTIfication part-------------------------------
+// the Get() function is not same as caffe in bosen, not modify temporarily
+// -----------------------------NOTIfication part end-------------------------------
 Caffe& Caffe::Get() {
   if (!thread_instance_.get()) {
     thread_instance_.reset(new Caffe());
@@ -50,11 +52,12 @@ void GlobalInit(int* pargc, char*** pargv) {
 }
 
 #ifdef CPU_ONLY  // CPU-only Caffe.
-
+// -----------------------------modification part-------------------------------
+// Add varialbe phases_(NULL) in
 Caffe::Caffe()
     : random_generator_(), mode_(Caffe::CPU),
-      solver_count_(1), root_solver_(true) { }
-
+      solver_count_(1), root_solver_(true), phases_(NULL) { }
+// -----------------------------modification part end-------------------------------
 Caffe::~Caffe() { }
 
 void Caffe::set_random_seed(const unsigned int seed) {
@@ -94,10 +97,11 @@ void* Caffe::RNG::generator() {
 }
 
 #else  // Normal GPU + CPU Caffe.
-
+// -----------------------------modification part-------------------------------
+// Add varialbe phases_(NULL) in
 Caffe::Caffe()
     : cublas_handle_(NULL), curand_generator_(NULL), random_generator_(),
-    mode_(Caffe::CPU), solver_count_(1), root_solver_(true) {
+    mode_(Caffe::CPU), solver_count_(1), root_solver_(true), phases_(NULL) {
   // Try to create a cublas handler, and report an error if failed (but we will
   // keep the program running as one might just want to run CPU code).
   if (cublasCreate(&cublas_handle_) != CUBLAS_STATUS_SUCCESS) {
@@ -111,7 +115,7 @@ Caffe::Caffe()
     LOG(ERROR) << "Cannot create Curand generator. Curand won't be available.";
   }
 }
-
+// -----------------------------modification part end-------------------------------
 Caffe::~Caffe() {
   if (cublas_handle_) CUBLAS_CHECK(cublasDestroy(cublas_handle_));
   if (curand_generator_) {
