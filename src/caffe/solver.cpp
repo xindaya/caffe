895b58676facb642f5f249571337c6f4b82bb9a3
yangzq08 <yzqzoom@163.com>
2015-09-23 17:26:02 +0800

    solver bug fix

--- src/caffe/solver.cpp
+++ src/caffe/solver.cpp
@@ -254,7 +254,7 @@ void Solver<Dtype>::InitTestNets() {
     if (Caffe::root_solver()) {
       //test_nets_[i].reset(new Net<Dtype>(net_params[i]));
 	  //test_nets_[i].reset(new Net<Dtype>(net_params[i], thread_id_, i));
-	  test_nets_[i].reset(new Net<Dtype>(net_params[i], this, thread_id_, i));
+	  test_nets_[i].reset(new Net<Dtype>(net_params[i], this->test_nets_[i].get(), thread_id_, i));
     } else {
       test_nets_[i].reset(new Net<Dtype>(net_params[i], root_solver_->test_nets_[i].get()));
     }
